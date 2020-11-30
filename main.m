cac95299df5ad2d5103e7304e970717dd80ed9f1
bowrango <mjb1085@wildcats.unh.edu>
2020-11-30 18:07:46 -0500

    inital commit for outline

---
+++ main.m
@@ -0,0 +1,17 @@
+%% Intro
+
+% This main script will outline the story for our animation. It will
+% implement various transformation functions in loops to animate a character.
+
+% Each of us will be responsible for one of these scenes. Once we figure out a 
+% story, we should include the expected inputs and outputs.
+
+%% Animation
+
+% SCENE 1 
+
+% SCENE 2
+
+% SCENE 3
+
+% SCENE 4
