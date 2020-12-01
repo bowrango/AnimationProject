f7d5734d685a19cbe35f24db01bc2a338c1b4749
cts1005 <70669910+cts1005@users.noreply.github.com>
2020-12-01 18:59:36 -0500

    Added main.mlx, and SunnyMariolevel

--- main.m
+++ main.m
@@ -9,6 +9,49 @@
 %% Animation
 
 % SCENE 1 
+% (Panning Camera) Large background, smaller view follows mario
+% Background:SunnyMarioLevel.jpg
+% est Duation:28 seconds 
+for i = 0:30
+
+% Mario standing still and further away mushroom. (2 Seconds)
+% No transformations, Stand still frame  
+% Standing Mario, Mushroom
+
+% Mario does a flip in and air and lands back down (3 seconds)
+% Mario shifts and rotates
+% Asset(s): Standing Mario, Jumping Mario, Mushroom
+
+% Mario stands in place and Mushroom shimmies offscreen (3 seconds)      
+% Mushroom shifts offscreen (shifts)
+% Asset(s): Standing Mario, Mushroom
+
+% Mario runs after mushroom and camera follows (3 seconds)
+% Mario shifts right, and rotates clockwise and counterclockwise looks like a wobble
+% Asset(s): Standing Mario, Jumping Mario, 
+
+% Brown Goomba walks toward Mario, Mario stops goes backward, and jumps on it(5 seconds) 
+% Goomba reflections and shifts, Mario shifts 
+% Asset(s): Standing Mario, jumping Mario, Big footed Goomba
+
+% Mario reaches big gap, stops in place, jumps up and down, then jumps over gap (5 seconds)
+% Mario shifts
+% Asset(s): Standing Mario, Jumping Mario
+
+% Mario reaches the not moving mushroom, Mario transforms into a red goomba (3 seconds)
+% Mario shifts and morphs(standing Mario-> Big footed Goomba)
+% Asset(s): Standing Mario, Mushroom, Big footed Goomba
+ 
+% Goomba Mario stops and wobbles in place, and proceeds toward the end of level (4 seconds)
+% Goomba Mario Shears left and right (wobble), Reflections and shifts after
+% Asset(s): Big footed Goomba
+
+% Goomba Mario goes down pipe (2 seconds)
+% Shears downward like a pankcake.
+% Asset(s): Red Goomba
+end
+
+
 
 % SCENE 2
 
