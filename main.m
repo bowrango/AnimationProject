cc452f124fdb637c58e7cab0a1d075b8379e474f
bowrango <mjb1085@wildcats.unh.edu>
2020-12-02 18:49:38 -0500

    Add Tim's scene to main and delete AnimationTim.m

--- main.m
+++ main.m
@@ -7,6 +7,7 @@
 % story, we should include the expected inputs and outputs.
 
 %% SCENE 1 
+
 % (Panning Camera) Large background, smaller view follows mario
 % Background:SunnyMarioLevel.jpg
 % est Duation:28 seconds 
@@ -51,7 +52,97 @@ end
 
 %% SCENE 2
 
+%Scene 2: Tim's "Underworld" level
 
+%%Transition -----------
+
+%pull in new background - have mario walk into the castle gate/door
+
+%have Mario appear in the dungeon...falling through the roof? 
+%pretty classic Mario
+
+%%Koops's appear ---------
+
+%pull in 2 or 3 koopa enemies
+
+%for i = 
+%have the koopa's move together towards Mario then stop
+%end
+
+%for i =
+%have mario move to jump on their heads
+    %for j = 
+    %each squished guy can fly off spinning
+        %for k =
+        %spin a little bit
+        %end
+    %end
+%end
+
+%%Mario Get's Firepower ---------
+
+%for i = 
+%Mario moves forward
+%end
+
+%Mario jumps to bump a block
+
+%for i = 1:3
+%a Firepower flower expands out of the top of the block
+%end
+
+%for i = 
+%mario moves to left or right to make flower accessable
+% end
+
+%for i = 
+%Mario jumps up to the firepower flower
+%end
+
+%for i = 
+%Mario moves over and down to flower
+%end
+
+%for i = 1:5
+%Mario transforms into fireball Mario
+%end
+
+%for i= 
+%Mario jumps off
+    %for j = 
+    %flipping a little each loop
+    %end
+%end
+
+%%two more turtles appear --------------
+
+%for i = 
+%two fireballs go laterally
+    %for j = 
+    %fireballs oscillating up and down
+    %end
+    
+        %if any vector in fireball = any vector in turtle:
+        %fireball dissapear
+            %for k = 
+            %turtles spins off 
+            %end
+        %end
+        
+%end
+
+%%Victory Bounce -----------
+%for i = 
+%Mario does a couple of excited bounces
+%end
+
+%for i = 
+%Mario runs off to reach a green pipe
+%end
+
+%for i = 
+%Mario transforms into a flat line - simulating a drop into the next level.
+%end
 
 
 %% SCENE 3
